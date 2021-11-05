4df22edee4b90a1c4fa161b384e366d88b83f438
laurapascoalotte <90223873+laurapascoalotte@users.noreply.github.com>
2021-11-04 21:12:15 -0300

    Add files via upload

---
+++ Cadastro.js
@@ -0,0 +1,32 @@
+//Situação problema 
+//Cadastro de Evento 
+
+var hoje = 19
+var evento = 22
+
+if (hoje < evento) 
+{  
+     console.log("Evento permitido");
+} else  { 
+    console.log("Data inválida");
+}
+
+var Idade = 18 
+
+if ( Idade >= 18) {
+    console.log ("Cadastro permitido!");
+} else
+{
+    console.log ("Idade não permitida");
+}
+
+var Participantes= 50; 
+
+if (Participantes >= 100) {
+    console.log ("Limite Exedido");
+} else 
+{
+    console.log ("Cadastro realizado com sucesso!");
+}
+
+
